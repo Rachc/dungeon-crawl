fdc5ededaf050ca34cc0eae4592d5d68b96ee651
Rachel Curioso <rachel.curioso@gmail.com>
2020-03-14 18:46:30 -0300

    raised everything

--- lib/dungeon_crawl/cli/base_commands.ex
+++ lib/dungeon_crawl/cli/base_commands.ex
@@ -1,6 +1,21 @@
 defmodule DungeonCrawl.CLI.BaseCommands do
   alias Mix.Shell.IO, as: Shell
 
+  def ask_for_option(options) do
+    try do
+      options
+      |> display_options
+      |> generate_question
+      |> Shell.prompt
+      |> parse_answer!
+      |> find_option_by_index!(options)
+    rescue
+      e in DungeonCrawl.CLI.InvalidOptionError ->
+        display_error(e)
+        ask_for_option(options)
+    end
+  end
+
   def display_options(options) do
     options
     |> Enum.with_index(1)
@@ -14,8 +29,21 @@ defmodule DungeonCrawl.CLI.BaseCommands do
     "Wich one? [#{options}]\n"
   end
 
-  def parse_answer(answer) do
-    {option, _} = Integer.parse(answer)
-    option - 1
+  def parse_answer!(answer) do
+    case Integer.parse(answer) do
+      :error -> raise DungeonCrawl.CLI.InvalidOptionError
+      {option, _} -> option - 1
+    end
+  end
+
+  def find_option_by_index!(index, options) do
+    Enum.at(options, index) || raise DungeonCrawl.CLI.InvalidOptionError
+  end
+
+  def display_error(e) do
+    Shell.cmd("clear")
+    Shell.error(e.message)
+    Shell.prompt("Press enter to continue")
+    Shell.cmd("clear")
   end
 end
--- lib/dungeon_crawl/cli/hero_choice.ex
+++ lib/dungeon_crawl/cli/hero_choice.ex
@@ -7,16 +7,7 @@ defmodule DungeonCrawl.CLI.HeroChoice do
     Shell.cmd("clear")
     Shell.info("Start by choosing your hero")
 
-    heroes = DungeonCrawl.Heroes.all
-    find_hero_by_index = &Enum.at(heroes, &1)
-
-    heroes
-    |> display_options
-    |> generate_question
-    |> Shell.prompt
-    |> parse_answer
-    |> find_hero_by_index.()
-    |> confirm_hero
+    ask_for_option(DungeonCrawl.Heroes.all)
   end
 
   defp confirm_hero(chosen_hero) do
